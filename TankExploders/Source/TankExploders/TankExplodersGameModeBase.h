e77b2abb45ae4e2d9ceec3683c2ce319fcd32f20
toastmuseum <Deolijus@gmail.com>
2017-04-20 23:08:55 -0400

    Landscape Manipulation and Initial Project Build

---
+++ TankExploders/Source/TankExploders/TankExplodersGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankExplodersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKEXPLODERS_API ATankExplodersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
